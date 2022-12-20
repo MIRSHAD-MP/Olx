7d78c284e8525c45cc5f5357e6f6899ec1a2590c
Mirshad Mp <109660034+MIRSHAD-MP@users.noreply.github.com>
2022-12-20 14:23:16 +0530

    change name

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,33 +1,39 @@
-import React, { useState,useContext } from 'react';
-
-import Logo from '../../olx-logo.png';
-import { FirebaseContext } from '../../store/Context';
-import { useHistory } from 'react-router-dom';
-import './Signup.css';
+import React, { useState, useContext } from "react";
+import Logo from "../../olx-logo.png";
+import { FirebaseContext } from "../../store/Context";
+import { useHistory } from "react-router-dom";
+import "./Signup.css";
 
 export default function Signup() {
-  const history = useHistory()
-  const [username,setUsername] = useState ('')
-  const [email,setEmail] = useState ('')
-  const [phone,setPhone] = useState ('')
-  const [password,setPassword] = useState ('')
+  const history = useHistory();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
 
-  const {firebase} = useContext (FirebaseContext)
+  const { firebase } = useContext(FirebaseContext);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    firebase.auth().createUserWithEmailAndPassword(email,password).then((result) => {
-      result.user.updateProfile({displayName:username}).then(() => {
-        firebase.firestore().collection('users').add({
-          id:result.user.uid,
-          username:username,
-          phone:phone
-        }).then(() => {
-          history.push("/login")
-        })
-      })
-    })
-  }
+    e.preventDefault();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        result.user.updateProfile({ displayName: username }).then(() => {
+          firebase
+            .firestore()
+            .collection("users")
+            .add({
+              id: result.user.uid,
+              username: username,
+              phone: phone,
+            })
+            .then(() => {
+              history.push("/login");
+            });
+        });
+      });
+  };
   return (
     <div>
       <div className="signupParentDiv">
@@ -39,7 +45,7 @@ export default function Signup() {
             className="input"
             type="text"
             value={username}
-            onChange={(e) => setUsername (e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
             id="fname"
             name="name"
             defaultValue="John"
@@ -51,7 +57,7 @@ export default function Signup() {
             className="input"
             type="email"
             value={email}
-            onChange={(e) => setEmail (e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             id="fname"
             name="email"
             defaultValue="John"
@@ -63,7 +69,7 @@ export default function Signup() {
             className="input"
             type="number"
             value={phone}
-            onChange={(e) => setPhone (e.target.value)}
+            onChange={(e) => setPhone(e.target.value)}
             id="lname"
             name="phone"
             defaultValue="Doe"
@@ -75,7 +81,7 @@ export default function Signup() {
             className="input"
             type="password"
             value={password}
-            onChange={(e) => setPassword (e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             id="lname"
             name="password"
             defaultValue="Doe"
